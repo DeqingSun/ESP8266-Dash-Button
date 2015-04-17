683c61cfbaad093052fdb886a50d8578df4d0732
Deqing Sun <iamseer@gmail.com>
2015-04-17 01:27:46 -0400

    fix j

--- ESP8266_button/app/src/main/java/org/thinkcreate/esp_reverse/rev_Class_j.java
+++ ESP8266_button/app/src/main/java/org/thinkcreate/esp_reverse/rev_Class_j.java
@@ -11,15 +11,15 @@ import java.net.UnknownHostException;
  * Created by sundeqing on 4/16/15.
  */
 public class rev_Class_j {
-    private DatagramSocket a;
-    private volatile boolean b;
+    private DatagramSocket mDatagramSocket;
+    private volatile boolean exceptionHappened;
 
     public rev_Class_j()
     {
         try
         {
-            this.a = new DatagramSocket();
-            this.b = false;
+            this.mDatagramSocket = new DatagramSocket();
+            this.exceptionHappened = false;
             return;
         }
         catch (SocketException localSocketException)
@@ -30,61 +30,53 @@ public class rev_Class_j {
 
     public void a()
     {
-        this.b = false;
+        this.exceptionHappened = false;
     }
 
     public void a(byte[][] paramArrayOfByte, String paramString, int paramInt, long paramLong)
     {
-        if ((paramArrayOfByte == null) || (paramArrayOfByte.length <= 0)) {}
-        label133:
-        for (int i=0;;)
-        {
-            return;
-            //int i = 0;
-            //if ((this.b) || (i >= paramArrayOfByte.length)) {}//todo
-            /*for (;;)
-            {
-                if (!this.b) {
-                    break label133;
-                }
-                b();
-                return;
-                if (paramArrayOfByte[i].length == 0)
+        if ((paramArrayOfByte != null) || (paramArrayOfByte.length > 0)) {
+            for (int i = 0; (!this.exceptionHappened) || (i < paramArrayOfByte.length) ; i++) {    //i:v0
+                if (paramArrayOfByte[i].length != 0)
                 {
-                    i += 1;
-                    break;
-                }
-                try
-                {
-                    DatagramPacket localDatagramPacket = new DatagramPacket(paramArrayOfByte[i], paramArrayOfByte[i].length, InetAddress.getByName(paramString), paramInt);
-                    this.a.send(localDatagramPacket);
                     try
                     {
-                        Thread.sleep(paramLong);
+                        DatagramPacket localDatagramPacket = new DatagramPacket(paramArrayOfByte[i], paramArrayOfByte[i].length, InetAddress.getByName(paramString), paramInt);
+                        this.mDatagramSocket.send(localDatagramPacket);
+                        try
+                        {
+                            Thread.sleep(paramLong);
+                        }
+                        catch (InterruptedException paramArrayOfByte_exp)
+                        {
+                            paramArrayOfByte_exp.printStackTrace();
+                            this.exceptionHappened = true;
+                            break;
+                        }
                     }
-                    catch (InterruptedException paramArrayOfByte_exp)
+                    catch (UnknownHostException paramArrayOfByte_exp1)
                     {
-                        paramArrayOfByte_exp.printStackTrace();
-                        this.b = true;
+                        paramArrayOfByte_exp1.printStackTrace();
+                        this.exceptionHappened = true;
+                        break;
+                    }
+                    catch (IOException paramArrayOfByte_exp2)
+                    {
+                        paramArrayOfByte_exp2.printStackTrace();
+                        this.exceptionHappened = true;
+                        break;
                     }
                 }
-                catch (UnknownHostException paramArrayOfByte_exp1)
-                {
-                    paramArrayOfByte_exp1.printStackTrace();
-                    this.b = true;
-                }
-                catch (IOException paramArrayOfByte_exp2)
-                {
-                    paramArrayOfByte_exp2.printStackTrace();
-                    this.b = true;
-                }
-            }*/
+            }
+            if (this.exceptionHappened) {
+                b();
+            }
         }
     }
 
     public void b()
     {
-        this.a.close();
+        this.mDatagramSocket.close();
     }
 
     protected void finalize() throws Throwable {
