fe9592133b800e993030351ee3acd983c17184c7
Deqing Sun <iamseer@gmail.com>
2015-04-17 16:46:02 -0400

    a finished

--- ESP8266_button/app/src/main/java/org/thinkcreate/esp_reverse/rev_Class_a.java
+++ ESP8266_button/app/src/main/java/org/thinkcreate/esp_reverse/rev_Class_a.java
@@ -17,21 +17,20 @@ public class rev_Class_a
         if (paramInt > 63) {
             throw new RuntimeException("index > INDEX_MAX");
         }
-        byte[] localObject0 = rev_Class_l_maybe_util.b(paramChar);
-        this.b = localObject0[0];
-        this.c = localObject0[1];
-        rev_Class_m localObject1 = new rev_Class_m();
-        ((rev_Class_m)localObject1).update(rev_Class_l_maybe_util.a(paramChar));
-        ((rev_Class_m)localObject1).update(paramInt);
-        byte[] localObject2 = rev_Class_l_maybe_util.b((char)(int)((rev_Class_m)localObject1).getValue());
-        this.d = localObject2[0];
-        this.e = localObject2[1];
-        if ((this.c == 0) && (this.e == 0))
-        {
+        byte[] localByteArray = rev_Class_l_maybe_util.b(paramChar);//v0
+        this.b = localByteArray[0];
+        this.c = localByteArray[1];
+        rev_Class_m localObjectClassM = new rev_Class_m();   //v0
+        localObjectClassM.update(rev_Class_l_maybe_util.a(paramChar));
+        localObjectClassM.update(paramInt);
+        byte[] localByteArray2 = rev_Class_l_maybe_util.b((char)(int)localObjectClassM.getValue());//v0
+        this.d = localByteArray2[0];
+        this.e = localByteArray2[1];
+        if ((this.c == 0) && (this.e == 0)) {
             this.a = ((byte)(paramInt | 0x1940));
-            return;
+        }else {
+            this.a = ((byte) (paramInt | 0x1900));
         }
-        this.a = ((byte)(paramInt | 0x1900));
     }
 
     public byte[] a()
@@ -41,21 +40,16 @@ public class rev_Class_a
 
     public String toString()
     {
-        StringBuilder localStringBuilder = new StringBuilder();
-        byte[] arrayOfByte = a();
-        int i = 0;
-        for (;;)
-        {
-            if (i >= 6) {
-                return localStringBuilder.toString();
-            }
+        StringBuilder localStringBuilder = new StringBuilder(); //v1
+        byte[] arrayOfByte = a();   //v2
+        for (int i=0;i<6;i++){
             String str = rev_Class_l_maybe_util.b(arrayOfByte[i]);
             localStringBuilder.append("0x");
             if (str.length() == 1) {
                 localStringBuilder.append("0");
             }
             localStringBuilder.append(str).append(" ");
-            i += 1;
         }
+        return localStringBuilder.toString();
     }
 }
