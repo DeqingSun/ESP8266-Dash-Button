5ca9106b25291a71dfe5965477a8102ef9cda1b0
Deqing Sun <iamseer@gmail.com>
2015-04-17 03:07:46 -0400

    fix most of i, need to fix h first and comeback

--- ESP8266_button/app/src/main/java/org/thinkcreate/esp_reverse/rev_Class_i.java
+++ ESP8266_button/app/src/main/java/org/thinkcreate/esp_reverse/rev_Class_i.java
@@ -7,29 +7,46 @@ package org.thinkcreate.esp_reverse;
 class rev_Class_i
         extends Thread
 {
-    rev_Class_i(rev_Class_h paramh) {}
+    private rev_Class_h a;
+
+    rev_Class_i(rev_Class_h paramh) {
+        a=paramh;
+    }
 
     public void run()
-    {//todo
-  /*      long l = System.currentTimeMillis();
-        int i = (byte)(rev_Class_h.a(this.a).length() + rev_Class_h.b(this.a).length());
-        int j = rev_Class_h.c(this.a).a();
-        if (j == i)
-        {
-            i = (int)(46000L - (System.currentTimeMillis() - l));
-            if (i >= 0) {}
-        }
-        for (;;)
-        {
-            this.a.a();
-            rev_Class_h.c(this.a).c();
-            return;
-            rev_Class_h.c(this.a).a(i);
-            rev_Class_h.a(this.a, true);
-            continue;
-            if (j != -128) {
-                break;
+    {
+        long l = System.currentTimeMillis();    //l:v0
+        int i = (byte)(rev_Class_h.a(this.a).length() + rev_Class_h.b(this.a).length());    //i:v2 paramh //recheck after fix h TODO
+
+        while(true) {
+
+            int j = rev_Class_h.c(this.a).a();  //j:v3 //recheck after fix h TODO
+            if (j == i) {
+                i = (int) (46000L - (System.currentTimeMillis() - l));
+                if (i >= 0) {
+                    rev_Class_h.c(this.a).a(i);
+                    rev_Class_h.a(this.a, true);
+                    break;
+                }else{
+                    break;
+                }
+            } else {
+                if (j != -128) {
+                    continue;
+                }else{
+                    break;
+
+                }
+
             }
-        }*/
+
+
+        }
+
+
+        this.a.a();
+        rev_Class_h.c(this.a).c();
+
+
     }
 }
