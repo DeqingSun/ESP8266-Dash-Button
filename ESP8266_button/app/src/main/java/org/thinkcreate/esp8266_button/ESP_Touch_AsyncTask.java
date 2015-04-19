95423926a1c40a41358ea04d3831c5e0d33bab92
Deqing Sun <iamseer@gmail.com>
2015-04-19 19:24:56 -0400

    add text on UI

--- ESP8266_button/app/src/main/java/org/thinkcreate/esp8266_button/ESP_Touch_AsyncTask.java
+++ ESP8266_button/app/src/main/java/org/thinkcreate/esp8266_button/ESP_Touch_AsyncTask.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.TextView;
 
 import org.thinkcreate.esp_reverse.rev_Class_h_ESP_TOUCH_Main;
 
@@ -12,22 +13,24 @@ import org.thinkcreate.esp_reverse.rev_Class_h_ESP_TOUCH_Main;
  */
 public class ESP_Touch_AsyncTask extends AsyncTask<Void,Void,Boolean>
     {
-        private Activity b;
+        private Activity parentActivity;
         private ProgressDialog c;
         private final String ssidStr;
         private final String passwordStr;
         private boolean g;
         private static final String TAG = "ESP_Touch_AsyncTask";
         private rev_Class_h_ESP_TOUCH_Main ESPSender;
+        private TextView mTextView;
 
         public ESP_Touch_AsyncTask(MainActivity paramEspTouchActivity, Activity paramActivity, String ssid, String password)
         {
-            this.b = paramActivity;
+            this.parentActivity = paramActivity;
             this.g = false;
             this.ssidStr = ssid;
             this.passwordStr = password;
             this.ESPSender=new rev_Class_h_ESP_TOUCH_Main(ssid, password);
           //  this.d = new activity_C_class_maybe_handle(this.ssidStr, this.passwordStr);
+            mTextView = (TextView) parentActivity.findViewById(R.id.textView1);
         }
 
     protected Boolean a(String... paramVarArgs)
@@ -66,6 +69,12 @@ public class ESP_Touch_AsyncTask extends AsyncTask<Void,Void,Boolean>
         }*/
         Log.d(TAG, "Cancelled");
         ESPSender.a();
+        parentActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mTextView.setText("Cancelled");
+            }
+        });
     }
 
 
@@ -83,13 +92,27 @@ public class ESP_Touch_AsyncTask extends AsyncTask<Void,Void,Boolean>
         this.c.setOnCancelListener(this);
         this.c.show();*/
         Log.d(TAG, "PRE EXE");
+        parentActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mTextView.setText("SETTING");
+            }
+        });
     }
 
         @Override
         protected void onPostExecute(Boolean result) {
             Log.d(TAG, "POST");
             Log.d(TAG, result?"SET OK":"SET Failed");
+            final boolean result2=result;
+            parentActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mTextView.setText(result2?"SET OK":"SET Failed");
+                }
+            });
         }
+
 
 
     }
